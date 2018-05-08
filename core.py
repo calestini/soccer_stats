86811c5e2bcd7c5ce6f4b55966c9eecc15ffa143
calestini <calestini27@gmail.com>
2018-05-07 23:36:48 -0400

    Poisson prob for scores

--- core.py
+++ core.py
@@ -4,7 +4,32 @@
 from sshtunnel import SSHTunnelForwarder
 import psycopg2
 import pandas as pd
+import numpy as np
 import json
+import scipy
+from sklearn.utils.extmath import cartesian
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+code_toggle = '''
+<script>
+    code_show=true;
+
+    function code_toggle() {
+         if (code_show){
+             $('div.input').hide();
+            } else {
+             $('div.input').show();
+            }
+         code_show = !code_show
+    }
+
+    $( document ).ready(code_toggle);
+</script>
+
+The raw code for this IPython notebook is by default hidden for easier reading.
+To toggle on/off the raw code, click <a href="javascript:code_toggle()">here</a>.'''
+
 
 def direct_query(querystring, settings_json='../settings.json'):
     with open(settings_json) as f:
@@ -26,6 +51,7 @@ def direct_query(querystring, settings_json='../settings.json'):
         conn.close()
     return results
 
+
 def events(settings_json = '../settings.json', league=8):
     with open(settings_json) as f:
         settings = json.load(f, strict=False)
@@ -34,6 +60,126 @@ def events(settings_json = '../settings.json', league=8):
     event_data = direct_query(querystring, settings_json = settings_json)
     return event_data
 
+
+def probability_matrix(events, hometeam, awayteam, goals_list, season='2016/2017', save_image=False):
+    """
+    :events: dataframe with fixture/events data
+    :return: probability matrix with all combinations
+    """
+    events_season = events[events['season'].isin(season)]
+    dedup_events = events_season[[
+            'fixture_id','localname', 'localteam_score','visitorteam_score',
+            'visitorname','season'
+        ]].drop_duplicates()
+
+    average_home = np.sum(dedup_events['localteam_score'])/len(dedup_events)
+    average_away = np.sum(dedup_events['visitorteam_score'])/len(dedup_events)
+
+    #attack strength
+    home_att_home = np.sum(dedup_events[dedup_events['localname']==hometeam]['localteam_score'])/len(dedup_events[dedup_events['localname']==hometeam])/average_home
+    #home_att_away = np.sum(dedup_events[dedup_events['visitorname']==hometeam]['visitorteam_score'])/len(dedup_events[dedup_events['visitorname']==hometeam])/average_away
+    #away_att_home = np.sum(dedup_events[dedup_events['localname']==awayteam]['localteam_score'])/len(dedup_events[dedup_events['localname']==awayteam])/average_home
+    away_att_away = np.sum(dedup_events[dedup_events['visitorname']==awayteam]['visitorteam_score'])/len(dedup_events[dedup_events['visitorname']==awayteam])/average_away
+
+    #defense strength
+    home_def_home = np.sum(dedup_events[dedup_events['localname']==hometeam]['visitorteam_score'])/len(dedup_events[dedup_events['localname']==hometeam])/average_away
+    #home_def_away = np.sum(dedup_events[dedup_events['visitorname']==hometeam]['localteam_score'])/len(dedup_events[dedup_events['visitorname']==hometeam])/average_home
+    #away_def_home = np.sum(dedup_events[dedup_events['localname']==awayteam]['visitorteam_score'])/len(dedup_events[dedup_events['localname']==awayteam])/average_away
+    away_def_away = np.sum(dedup_events[dedup_events['visitorname']==awayteam]['localteam_score'])/len(dedup_events[dedup_events['visitorname']==awayteam])/average_home
+
+    #expected goals
+    #goals home will score:
+    home_goals = home_att_home * away_def_away * average_home
+
+    #goals away will score;
+    away_goals = away_att_away * home_def_home * average_away
+
+    #poisson distrib.
+    poisson = scipy.stats.distributions.poisson
+    goals = goals_list
+    home_prob = poisson.pmf(goals, home_goals)
+    away_prob = poisson.pmf(goals, away_goals)
+
+    home_df = pd.DataFrame([home_prob,goals], index=['home_prob','home_score']).transpose()
+    away_df = pd.DataFrame([away_prob,goals], index=['away_prob','away_score']).transpose()
+
+    #probability matrix with all scores combinations
+    all_results = pd.DataFrame(cartesian([home_prob, away_prob]), columns = ['home_prob','away_prob']).merge(
+        home_df, on='home_prob', how='inner'
+    ).merge(
+        away_df, on='away_prob', how='inner'
+    )
+
+    home_prob = np.sum(
+        all_results[all_results['home_score'] > all_results['away_score']]['home_prob']*
+        all_results[all_results['home_score'] > all_results['away_score']]['away_prob']
+    )
+
+
+    tie_prob = np.sum(
+        all_results[all_results['home_score'] == all_results['away_score']]['home_prob']*
+        all_results[all_results['home_score'] == all_results['away_score']]['away_prob']
+    )
+
+
+    away_prob = np.sum(
+        all_results[all_results['home_score'] < all_results['away_score']]['home_prob']*
+        all_results[all_results['home_score'] < all_results['away_score']]['away_prob']
+    )
+
+    outcome = [home_prob, tie_prob, 1. - (home_prob+tie_prob)] # using 1.- to add to complete 100.00%
+
+    all_results['prob'] = all_results['home_prob']*all_results['away_prob']
+    prob_matrix = all_results.pivot('home_score', 'away_score', 'prob')
+
+    #plot
+    plt.figure(figsize=(16,7))
+    plt.subplot2grid((2, 4), (0, 0), rowspan=2, colspan=2)
+    sns.heatmap(prob_matrix, annot=True, fmt = '.2%', square=1, linewidth=1.)
+    plt.title('{0} vs. {1}\n'.format(hometeam, awayteam), fontsize=17)
+    plt.xlabel(awayteam)
+    plt.ylabel(hometeam)
+
+    ax = plt.subplot2grid((2, 4), (0, 2))
+    plt.title('Goal Distribution\n', fontsize=17)
+    sns.countplot(dedup_events[dedup_events['localname'] == hometeam]['localteam_score'], color='#f15b24')
+    plt.xlabel('Hometeam: {0}'.format(hometeam))
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.yticks([], [])
+    plt.ylabel('')
+
+    ax1 = plt.subplot2grid((2, 4), (1, 2))
+    sns.countplot(dedup_events[dedup_events['visitorname'] == awayteam]['visitorteam_score'], color='#363e4f')
+    plt.xlabel('Awayteam: {0}'.format(awayteam))
+    ax1.spines['top'].set_visible(False)
+    ax1.spines['left'].set_visible(False)
+    ax1.spines['right'].set_visible(False)
+    plt.yticks([], [])
+    plt.ylabel('')
+
+    ax2 = plt.subplot2grid((2, 4), (0, 3), rowspan=2)
+    plt.title('3-way odds\n', fontsize=17)
+    plt.bar(range(len(outcome)),outcome, color=['#f15b24','grey','#363e4f'])
+    plt.xticks(range(len(outcome)), [hometeam, 'tie',awayteam])
+    ax2.spines['top'].set_visible(False)
+    ax2.spines['left'].set_visible(False)
+    ax2.spines['right'].set_visible(False)
+    plt.yticks([], [])
+    plt.ylabel('')
+    plt.text(-0.2, outcome[0]+0.01, '{:.1%}'.format(outcome[0]), fontsize=12)
+    plt.text(0.8, outcome[1]+0.01, '{:.1%}'.format(outcome[1]), fontsize=12)
+    plt.text(1.8, outcome[2]+0.01, '{:.1%}'.format(outcome[2]), fontsize=12)
+
+    plt.tight_layout()
+    if save_image:
+        plt.savefig('{0}_{1}.png'.format(hometeam, awayteam))
+    plt.show()
+
+    return prob_matrix
+
+################################################################################
 #reading settings file
 class queryTorneo(object):
 
